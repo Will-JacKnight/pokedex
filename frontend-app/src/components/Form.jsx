9fcdd363f8bf587c385ce3749c8816f5d51a23e0
yashkumar03 <ynk24@ic.ac.uk>
2024-12-07 11:57:46 +0000

    Fix 404 error for signup page

--- frontend-app/src/components/Form.jsx
+++ frontend-app/src/components/Form.jsx
@@ -1,6 +1,8 @@
 import { ClipLoader } from "react-spinners"
+import { useNavigate } from 'react-router-dom';
 
 function Form({handlePasswordChange, handleUsernameChange, handleSubmit, username, password, login, credentialsError, loading}) {
+    const navigate = useNavigate(); // used to navigate to other pages
     // used to override the background color and position of the loader
     const ovveride ={
         backgroundColor: "white",
@@ -8,6 +10,19 @@ function Form({handlePasswordChange, handleUsernameChange, handleSubmit, usernam
         bottom: "25px",
         left: "200px",
       }
+
+    // necessary to navigate() to signup/login page instead of using href in anchor tag because impaas does not allow redirecting to other routes
+    // that are not in the backend through href
+    // using navgiate() instead of href makes it possible
+    function handleSignupClick(e) {
+        e.preventDefault();
+        navigate('/signup');
+    }
+    
+    function handleLoginClick(e) {
+        e.preventDefault();
+        navigate('/login');
+    }
     
       // returning the JSX for the form containg the input fields and button
     return (
@@ -36,8 +51,8 @@ function Form({handlePasswordChange, handleUsernameChange, handleSubmit, usernam
                     {login ? "Login" : "Sign Up"}
                 </button>
                 {/* contidionally rendering the error messages */}
-                {login && <p className='not-registered'><i>Not registered? </i><a href='/signup'><i>Sign Up</i></a></p>}
-                {!login && <p className='not-registered'><i>Already registered? </i><a href='/login'><i>Login</i></a></p>}
+                {login && <p className='not-registered'><i>Already Registered? <span className="underline" onClick={handleSignupClick}>Sign Up</span></i></p>}
+                {!login && <p className='not-registered'><i>Already Registered? <span className="underline" onClick={handleLoginClick}>Login</span></i></p>}
             
                 {/* conditionally rendering the loader */}
                 {loading && <ClipLoader
